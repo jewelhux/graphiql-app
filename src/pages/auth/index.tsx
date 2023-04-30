9367e02f12b76617ba23e6d36ef27a007265c1ad
Jik789 <stix.jik@gmail.com>
2023-04-30 16:23:01 +0300

    feat: base page and mui

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material';
+
+const Auth = () => {
+  return (
+    <>
+      <h1>Выберите действие</h1>
+      <Button variant="contained">Войти</Button>
+      <Button variant="contained">Зарегистрироваться</Button>
+    </>
+  );
+};
+
+export default Auth;
