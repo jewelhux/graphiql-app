e3a8094d77316d96e4a5271c65058a0a19fcccd2
Jik789 <stix.jik@gmail.com>
2023-05-01 09:01:29 +0300

    feat: pressed the footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import Head from 'next/head';
 import { FC, ReactNode } from 'react';
 
@@ -9,14 +10,14 @@ interface ILayoutProps {
 }
 
 const Layout: FC<ILayoutProps> = ({ children }) => (
-  <>
+  <div className="test">
     <Head>
       <title>RSFINAL</title>
     </Head>
     <Header />
-    {children}
+    <Box style={{ flex: 1 }}>{children}</Box>
     <Footer />
-  </>
+  </div>
 );
 
 export default Layout;
