1c40a8042321fe07176b105b74f6b26a692498d4
Jik789 <stix.jik@gmail.com>
2023-05-02 16:16:39 +0300

    feat: testing bad and good page

---
+++ src/pages/auth/bad.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const Bad: FC = () => {
+  return <div>Все плохо, ты не прошёл</div>;
+};
+
+export default Bad;
---
+++ src/pages/auth/good.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const Good: FC = () => {
+  return <div>Все хорошо, ты прошёл</div>;
+};
+
+export default Good;
