9fec5c7e4385238b1b54c9cee73dcf15c0517431
Jik789 <stix.jik@gmail.com>
2023-05-03 09:07:30 +0300

    feat: add hook useAuth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 };
 
