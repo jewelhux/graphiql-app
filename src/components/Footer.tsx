f2f185c0f23937c9dc967439084e449126a7301b
OKitel <kitelolga@gmail.com>
2023-05-04 11:18:18 +0200

    refactor: transfer Footer to antd

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,33 @@
+import { Layout, Row, Col, Typography, Image, Button } from 'antd';
 import { ReactElement } from 'react';
-import { Toolbar, Container, Typography, Box, Avatar, Link, Divider } from '@mui/material';
 
 function Footer(): ReactElement {
   return (
-    <Box bgcolor={'backround.default'}>
-      <Divider />
-      <Container>
-        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Box sx={{ display: 'flex', columnGap: '5px' }}>
-            <Link href="https://github.com/Jik789">
-              <Avatar alt="JiK" src="https://avatars.githubusercontent.com/u/38877564?v=4" />
-            </Link>
-            <Link href="https://github.com/okitel">
-              <Avatar alt="okitel" src="https://avatars.githubusercontent.com/u/79774026?v=4" />
-            </Link>
-            <Link href="https://github.com/Syderi">
-              <Avatar alt="Syderi" src="https://avatars.githubusercontent.com/u/107023048?v=4" />
-            </Link>
-          </Box>
-          <Typography sx={{ fontWeight: '600' }}>2023</Typography>
-          <Link href="https://rs.school/index.html">
-            <Avatar
-              sx={{ objectFit: 'contain', width: '100%', height: '50px' }}
-              alt="JiK"
-              src="https://rs.school/images/partners/logo-rs.svg"
-            />
-          </Link>
-        </Toolbar>
-      </Container>
-    </Box>
+    <Layout.Footer>
+      <Row justify="space-between">
+        <Col>
+          <Row>
+            <Button type="link" href="https://github.com/Jik789">
+              <Image alt="JiK" src="https://Images.githubusercontent.com/u/38877564?v=4" />
+            </Button>
+            <Button type="link" href="https://github.com/okitel">
+              <Image alt="okitel" src="https://Images.githubusercontent.com/u/79774026?v=4" />
+            </Button>
+            <Button type="link" href="https://github.com/Syderi">
+              <Image alt="Syderi" src="https://Images.githubusercontent.com/u/107023048?v=4" />
+            </Button>
+          </Row>
+        </Col>
+        <Col>
+          <Typography.Title level={2}>2023</Typography.Title>
+        </Col>
+        <Col>
+          <Button type="link" href="https://rs.school/index.html">
+            <Image alt="JiK" src="https://rs.school/images/partners/logo-rs.svg" />
+          </Button>
+        </Col>
+      </Row>
+    </Layout.Footer>
   );
 }
 
