973d91ee514fda579582092409412bcfbf57bf5e
OKitel <kitelolga@gmail.com>
2023-05-04 15:55:45 +0200

    refactor: transfer Form to ant

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,27 +1,9 @@
-import { FC, useState } from 'react';
-import {
-  TextField,
-  Box,
-  Container,
-  Avatar,
-  Typography,
-  Button,
-  Grid,
-  Paper,
-  InputAdornment,
-  IconButton,
-  FormControl,
-  InputLabel,
-  OutlinedInput,
-  FormHelperText,
-} from '@mui/material';
-import LockIcon from '@mui/icons-material/Lock';
-import HowToRegIcon from '@mui/icons-material/HowToReg';
-import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { FC } from 'react';
 import { useRouter } from 'next/router';
-import { useForm } from 'react-hook-form';
 import { Auth } from '@/types/enum';
 import { EMAIL_REGEXP, PASSWORD_REGEXP } from '@/utils/const';
+import { Form as AntForm, Input, Typography, Button, Col, Avatar } from 'antd';
+import { LockOutlined, UserAddOutlined } from '@ant-design/icons';
 
 interface IFormProps {
   variantAuth: string;
@@ -33,22 +15,18 @@ interface IFormData {
 }
 
 const Form: FC<IFormProps> = ({ variantAuth }) => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<IFormData>({ mode: 'onChange', reValidateMode: 'onChange' });
-
   const { push } = useRouter();
-  const [showPassword, setShowPassword] = useState(false);
 
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const [form] = AntForm.useForm();
 
-  const onSubmit = (data: IFormData) => {
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  const onFinish = (data: IFormData) => {
     alert(data.password);
     alert(data.email);
-    reset();
+    onReset();
   };
 
   const handleLinkToOtherAuth = () => {
@@ -56,110 +34,52 @@ const Form: FC<IFormProps> = ({ variantAuth }) => {
   };
 
   return (
-    <>
-      <Container
-        component="main"
-        maxWidth="md"
-        sx={{
-          display: 'flex',
-          flexGrow: '1',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Paper elevation={8} sx={{ padding: 5, marginBottom: 2, marginTop: 2, width: '100%' }}>
-          <Box
-            display="flex"
-            flexDirection={'column'}
-            sx={{
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              {variantAuth === Auth.signin ? <LockIcon /> : <HowToRegIcon />}
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              {variantAuth}
-            </Typography>
-            <Box component="form" noValidate sx={{ mt: 3 }} onSubmit={handleSubmit(onSubmit)}>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    {...register('email', {
-                      required: 'Please enter your email address',
-                      pattern: {
-                        value: EMAIL_REGEXP,
-                        message: 'Please enter a valid email address',
-                      },
-                    })}
-                    error={errors.email ? true : false}
-                    helperText={errors.email ? errors.email.message : ' '}
-                    fullWidth
-                    id="email"
-                    label={'email'}
-                    autoComplete="email"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <FormControl fullWidth>
-                    <InputLabel htmlFor="outlined-adornment-password">password*</InputLabel>
-                    <OutlinedInput
-                      endAdornment={
-                        <InputAdornment position="end">
-                          <IconButton
-                            aria-label="toggle password visibility"
-                            edge="end"
-                            onClick={handleClickShowPassword}
-                          >
-                            {showPassword ? <VisibilityOff /> : <Visibility />}
-                          </IconButton>
-                        </InputAdornment>
-                      }
-                      {...register('password', {
-                        required: 'Please enter your password',
-                        pattern: {
-                          value: PASSWORD_REGEXP,
-                          message:
-                            'Password must have min 8 chars (uppercase, lowercase, digit and special char)',
-                        },
-                      })}
-                      error={errors.password ? true : false}
-                      fullWidth
-                      label={'password'}
-                      type={showPassword ? 'text' : 'password'}
-                      id="password"
-                      autoComplete="new-password"
-                    />
-                  </FormControl>
-                </Grid>
-              </Grid>
-              <FormHelperText error id="accountId-error">
-                {errors.password ? errors.password.message : ' '}
-              </FormHelperText>
-
-              <Button
-                disabled={false}
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                {variantAuth}
-              </Button>
-              <Button
-                disabled={false}
-                fullWidth
-                sx={{ mt: 1, mb: 1 }}
-                onClick={handleLinkToOtherAuth}
-              >
-                {`GO TO ${variantAuth === Auth.signin ? Auth.signup : Auth.signin}`}
-              </Button>
-            </Box>
-          </Box>
-        </Paper>
-      </Container>
-    </>
+    <AntForm
+      name="sign in"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+    >
+      <Col>
+        <Avatar icon={variantAuth === Auth.signin ? <LockOutlined /> : <UserAddOutlined />} />
+        <Typography.Title level={2}>{variantAuth}</Typography.Title>
+        <AntForm.Item
+          label="Email"
+          name="Email"
+          rules={[
+            { required: true, message: 'Please enter your email!' },
+            {
+              pattern: EMAIL_REGEXP,
+              message: 'Please enter a valid email address',
+            },
+          ]}
+        >
+          <Input />
+        </AntForm.Item>
+        <AntForm.Item
+          label="Password"
+          name="password"
+          rules={[
+            { required: true, message: 'Please enter your password!' },
+            {
+              pattern: PASSWORD_REGEXP,
+              message:
+                'Password must have min 8 chars (uppercase, lowercase, digit and special char)',
+            },
+          ]}
+        >
+          <Input.Password />
+        </AntForm.Item>
+        <Button disabled={false} type="primary">
+          {variantAuth}
+        </Button>
+        <Button disabled={false} type="default" onClick={handleLinkToOtherAuth}>
+          {`GO TO ${variantAuth === Auth.signin ? Auth.signup : Auth.signin}`}
+        </Button>
+      </Col>
+    </AntForm>
   );
 };
 
