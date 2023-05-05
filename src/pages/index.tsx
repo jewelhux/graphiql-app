f596707da0ed73b7e29cbd84daf06a5e6523a67c
Syderi <syderi@yahoo.com>
2023-05-05 15:02:28 +0300

    fix: buildHTTPExecutor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,25 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { buildHTTPExecutor } from '@graphql-tools/executor-http';
+import { schemaFromExecutor } from '@graphql-tools/wrap';
 
 const Auth = () => {
   const { pathname } = useRouter();
+
+  const remoteExecutor = buildHTTPExecutor({
+    endpoint: 'https://rickandmortyapi.com/graphql',
+  });
+
+  (async () => {
+    const postsSubschema = {
+      schema: await schemaFromExecutor(remoteExecutor),
+      executor: remoteExecutor,
+    };
+
+    const fields = postsSubschema.schema.getQueryType()?.getFields();
+    const result = JSON.parse(JSON.stringify(fields));
+    alert(result);
+  })();
 
   return (
     <>
