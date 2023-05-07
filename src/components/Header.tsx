b8b3fd0c9c588a53c38b10461f90b692841b0db6
OKitel <kitelolga@gmail.com>
2023-05-07 21:58:55 +0200

    feat: add animation for header on scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { Typography, Button, Layout, Row, Col, Space } from 'antd';
 import { useAuth } from '@/pages/hooks/useAuth';
@@ -18,8 +18,28 @@ const Header: FC = () => {
     push('/');
   };
 
+  const next = document.getElementById('__next');
+
+  const headerColorChange = () => {
+    const header = document.querySelector('.header');
+    if (next && next.scrollTop > 0) {
+      header?.classList.remove('unscrolled');
+      header?.classList.add('scrolled');
+    } else {
+      header?.classList.remove('scrolled');
+      header?.classList.add('unscrolled');
+    }
+  };
+
+  useEffect(() => {
+    next?.addEventListener('scroll', headerColorChange);
+    return function cleanup() {
+      next?.removeEventListener('scroll', headerColorChange);
+    };
+  });
+
   return (
-    <AntHeader className="header">
+    <AntHeader className="header unscrolled">
       <Row justify="space-between" align="middle">
         <Col>
           <Title
