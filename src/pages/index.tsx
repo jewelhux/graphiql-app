30070d85c944da30b926e296d9724e794aaba0f8
Syderi <syderi@yahoo.com>
2023-05-22 17:33:35 +0300

    fix: add Link GraphQl

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,14 @@ import { Typography } from 'antd';
 import { useTranslation } from 'react-i18next';
 import Astronaut from '@/components/Astronaut';
 import { Row } from 'antd';
+import Link from 'next/link';
+import { useAuth } from '@/hooks/useAuth';
 
 const Auth = () => {
   const { Title } = Typography;
   const { pathname } = useRouter();
   const { t } = useTranslation();
+  const { isAuth } = useAuth();
 
   return (
     <>
@@ -18,6 +21,14 @@ const Auth = () => {
       <Title id="welcome" level={2}>
         {t('welcome.title')}
       </Title>
+      {isAuth && (
+        <Link
+          style={{ display: 'block', textAlign: 'center', fontWeight: 'bold' }}
+          href={'/graphi'}
+        >
+          GraphQl
+        </Link>
+      )}
       <Row justify="center">
         <Astronaut />
       </Row>
