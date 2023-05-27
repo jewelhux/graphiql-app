9f7ae4558e751240f64de2b4db79398f78efab4e
OKitel <kitelolga@gmail.com>
2023-05-27 03:55:30 +0200

    feat: add info on welcome page

--- src/locales/i18cfg.ts
+++ src/locales/i18cfg.ts
@@ -8,6 +8,23 @@ i18n.use(initReactI18next).init({
       translation: {
         welcome: {
           title: 'Welcome to GraphiQL',
+          desc: 'This application that serves as a captivating playground for GraphQL requests, allowing users to immerse themselves in the fascinating realm of SpaceX. Discover the wonders of space exploration, access mission details, rocket specifications, and launch histories with ease, all within an intuitive interface designed to bring the cosmos closer to your fingertips',
+        },
+        info: {
+          devTitle: 'Developers',
+          dev1Title: 'Ilya Danilov',
+          dev1Des: 'Team leader, project setup, firebase, logic router, redux',
+          dev2Title: 'Olga Kitel',
+          dev2Des: 'Design, localization, docs, refactoring, stabilization and optimization',
+          dev3Title: 'Eugeniy Kuzmin',
+          dev3Des: 'App structure, GraphQL editor, docs, form validation, redux',
+          countryRU: 'Russia',
+          countryPL: 'Poland',
+          schoolTitle: 'Rolling Scopes School',
+          courseTitle: 'React course',
+          courseDes:
+            'Free of charge learning\n Open to everyone\n Learning materials\n Certificate\n',
+          enrollBtn: 'Enroll',
         },
         auth: {
           redirect: 'Go to editor',
@@ -47,9 +64,25 @@ i18n.use(initReactI18next).init({
       translation: {
         welcome: {
           title: 'Добро пожаловать в GraphiQL',
+          desc: 'Приложение, которое является захватывающей площадкой для выполнения GraphQL-запросов, позволяющих пользователям окунуться в увлекательный мир SpaceX. Откройте для себя чудеса космического исследования, получите доступ к деталям миссий, спецификациям ракет и истории запусков! Всё это в интуитивно понятном интерфейсе, что делает исследование космоса доступным для всех',
+        },
+        info: {
+          devTitle: 'Разработчики',
+          dev1Title: 'Илья Данилов',
+          dev1Des: 'Лидер команды, настройка проекта, firebase, роутер, redux',
+          dev2Title: 'Ольга Китель',
+          dev2Des: 'Дизайн, перевод, стабилизация и оптимизация',
+          dev3Title: 'Евгений Кузьмин',
+          dev3Des: 'Архитектура, редактор GraphQL, документация, валидация формы, redux',
+          countryRU: 'Россия',
+          countryPL: 'Польша',
+          schoolTitle: 'Rolling Scopes School',
+          courseTitle: 'Курс по React',
+          courseDes: 'Бесплатный\n Доступен каждому\n Учебные материалы\n Сертификат',
+          enrollBtn: 'Записаться',
         },
         auth: {
-          redirect: 'Перейти в редактор',
+          redirect: 'Редактор',
           signIn: 'Вход',
           signUp: 'Регистрация',
           logout: 'Выход',
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { Typography } from 'antd';
 import { useTranslation } from 'react-i18next';
 import Astronaut from '@/components/Astronaut';
-import { Row } from 'antd';
+import { Row, Typography } from 'antd';
+import Info from '@/components/Info';
 
 const Auth = () => {
   const { Title } = Typography;
@@ -19,8 +19,14 @@ const Auth = () => {
         {t('welcome.title')}
       </Title>
       <Row justify="center">
+        <Title className="welcome-desc" level={5}>
+          {t('welcome.desc')}
+        </Title>
+      </Row>
+      <Row justify="center">
         <Astronaut />
       </Row>
+      <Info />
     </>
   );
 };
