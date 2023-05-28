54564a55ae687c157c7ec94c89252ee7c8120263
Syderi <syderi@yahoo.com>
2023-05-28 23:27:57 +0300

    fix: fix URL_GRAPH_GL

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -4,4 +4,4 @@ export const EMAIL_REGEXP =
 export const PASSWORD_REGEXP =
   /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&{}()\[\];'":])[A-Za-z\d@$!%*#?&:;(){}\[\]'"]{8,}$/;
 
-export const URL_GRAPH_GL = 'https://spacex-production.up.railway.app';
+export const URL_GRAPH_GL = 'https://spacex-production.up.railway.app/';
